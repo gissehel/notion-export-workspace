83f98e79e46539013729eb9bc6d7a3684523f536
gissehel <public-devgit-orcan@gissehel.org>
2023-08-26 17:51:06 +0200

    FEATURE : Update commander tool to add read_block

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { program } = require('commander');
 const { export_notion_workspace } = require('./export')
 const { export_pages } = require('./export')
 const { export_all_databases } = require('./export')
+const { read_backup } = require('./backup-reader')
 
 program
     .version('0.0.1')
@@ -38,6 +39,16 @@ program
     .option('-t, --token <token>', 'Notion token', process.env.NOTION_TOKEN)
     .action((options) => {
         export_all_databases(options.token, options.path)
+    })
+
+program
+    .command('read_block')
+    .description('Read a Notion block')
+    .option('-p, --path <path>', 'Export path', process.env.NOTION_EXPORT_PATH)
+    .option('-b, --block <block>', 'Block ID', process.env.BLOCK_ID)
+    .action((options) => {
+        console.log({ options })
+        read_backup(options.path, options.block)
     })
 
 program.parse(process.argv)
